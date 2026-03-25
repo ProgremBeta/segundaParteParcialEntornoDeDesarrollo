169438b800dc253144819d272f5fa849fcae23eb
Ignacio <151699660+ProgremBeta@users.noreply.github.com>
2026-03-25 00:57:52 +0000

    codigo del main.py corregido

--- main.py
+++ main.py
@@ -8,10 +8,9 @@ KEY = load_dotenv("APY_KEY")
 @app.get("/llm/{prompt}") 
 
 async def read_root(prompt): 
-
-# The client gets the API key from the environment variable `GEMINI_API_KEY`. client = genai.Client() 
-response = client.models.generate_content( 
-model="gemini-3-flash-preview", contents=prompt 
-) 
-print(response.text) 
-return {"Respuesta": response.text} 
+    # The client gets the API key from the environment variable `GEMINI_API_KEY`. client = genai.Client() 
+    response = client.models.generate_content( 
+    model="gemini-3-flash-preview", contents=prompt )
+    
+    print(response.text) 
+    return {"Respuesta": response.text} 
